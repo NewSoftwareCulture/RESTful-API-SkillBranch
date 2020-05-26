93c6c4e86d54aa128df6b0eb62a2ef88413db501
Vlad <vlados.pochta@gmail.com>
2020-05-26 22:19:26 +0300

    Add Logger

---
+++ src/middlewares/Logger.js
@@ -0,0 +1,38 @@
+const none = '\u001b[0m';
+const green = '\u001B[32m';
+const blue =  "\u001B[34m";
+const red = "\u001B[31m";
+const cyan = "\u001B[36m";
+const yellow = "\u001B[33m";
+
+const __DEV__ = true;
+
+const log = (arg = '') => {
+    if (__DEV__ === true) console.log(arg);
+};
+log.GET = (arg) => {
+    if (__DEV__ === true) console.log(`${green}[GET]\t${none}${arg}`);
+};
+log.POST = (arg) => {
+    if (__DEV__ === true) console.log(`${blue}[POST]\t${none}${arg}`);
+};
+log.PUT = (arg) => {
+    if (__DEV__ === true) console.log(`${cyan}[PUT]\t${none}${arg}`);
+};
+log.DELETE = (arg) => {
+    if (__DEV__ === true) console.log(`${red}[DELETE]\t${none}${arg}`);
+};
+log.work = (arg) => {
+    if (__DEV__ === true) console.log(`${yellow}[WORK]\t${none}${arg}`);
+}
+log.connect = (arg) => {
+    if (__DEV__ === true) console.log(`${green}[CONNECT]\t${none}${arg}`);
+}
+log.db = (arg) => {
+    if (__DEV__ === true) console.log(`${green}[MongoDB]\t${none}${arg}`);
+}
+log.ERROR = (arg) => {
+    if (__DEV__ === true) console.log(`${red}[ERROR]\t${arg}`);
+};
+
+module.exports = log;
