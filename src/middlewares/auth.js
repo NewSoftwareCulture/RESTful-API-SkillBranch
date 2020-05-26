7774683e81854557d2fe16738d9008e1516145ff
Vlad <vlados.pochta@gmail.com>
2020-05-26 03:10:12 +0300

    Upd auth
    Add test post

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,5 +1,12 @@
 import { AsyncRouter } from 'express-async-router';
+// import models from '../models/models';
+import User from '../models/User/User'
+// import bodyParser from 'body-parser';
 const router = AsyncRouter();
+
+// const User = models.User;
+
+
 
 router.get('/', async (req, res) => {
     res.status(400).send('Hello!');
@@ -9,6 +16,31 @@ router.get('/', async (req, res) => {
 router.get('/user', async (req, res) => {
     res.send('Hello User!');
     console.log('[GET] /api/auth/user');
+});
+
+router.post('/', async(req, res) => {
+    const userId = 1;
+    const firstName = req.body.firstName;
+    const lastName = req.body.lastName;
+    const email = req.body.email;
+    const password = req.body.password;
+    const accessToken = "110";
+    const refreshToken = "220";
+
+    
+    const result = {
+        userId,
+        firstName,
+        lastName,
+        email,
+        password,
+        accessToken,
+        refreshToken
+    }
+    const user = new User(result);
+
+    user.save().then(() => console.log('User created!'));
+    res.json(result);
 });
 
 module.exports = router;
